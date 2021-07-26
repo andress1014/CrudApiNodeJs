fb31f9a6e59f89625b4270ba216f16c8b188c984
andress1014 <andres1014274122@gmail.com>
2021-07-26 12:55:27 -0500

    actualización con cambios de token para usuario

--- src/database.js
+++ src/database.js
@@ -3,7 +3,10 @@ import mongoose from 'mongoose'
 mongoose.connect("mongodb://Localhost/companydb" , {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    userFindAndModify: true
+    userFindAndModify: true,
+    useCreateIndex: true
 })
     .then(db => console.log('DB is connected'))
     .catch(error => console.log(error))
+
+    
