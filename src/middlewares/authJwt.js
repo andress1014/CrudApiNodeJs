acd80baf056cd9e57ae77fff290230537e0d42b6
andress1014 <andres1014274122@gmail.com>
2021-07-27 19:39:53 -0500

    Ultimos cambios

---
+++ src/middlewares/authJwt.js
@@ -0,0 +1,58 @@
+import jwt from 'jsonwebtoken'
+import config from '../config'
+import User from '../models/User'
+import Role from '../models/Role'
+
+
+export const verifyToken = async (req, res, next) => {
+    const token = req.headers["x-access-token"]
+
+    console.log(token)
+
+    if (!token) return res.status(403).json({ message: "No token provided" })
+
+    try {
+        const decoded = jwt.verify(token, config.SECRET)
+        req.userId = decoded.id
+
+        const user = await User.findById(req.userId, { password: 0 })
+        console.log(user)
+        if (!user) return res.status(404).json({ message: 'no user found' })
+
+        next()
+    } catch (error) {
+        return res.status(401).json({ message: 'Unathorize' })
+    }
+
+}
+
+export const isModerator = async (req, res, next) => {
+    const user = await User.findById(req.userId)
+    const roles = await Role.find({ _id: { $in: user.roles } })
+    console.log(roles)
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'moderador') {
+            next()
+            return
+        }
+
+    }
+
+    return res.status(403).json({ message: "Require Moderator role" })
+}
+
+
+export const isAdmin = async (req, res, next) => {
+    const user = await User.findById(req.userId)
+    const roles = await Role.find({ _id: { $in: user.roles } })
+    console.log(roles)
+    for (let i = 0; i < roles.length; i++) {
+        if (roles[i].name === 'admin') {
+            next()
+            return
+        }
+
+    }
+
+    return res.status(403).json({ message: "Require Admin role" })
+}
--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 const router = Router()
 
 import * as productsCtrl from '../controllers/products.controller'
+import { authJwt } from '../middlewares';
 
-router.post('/', productsCtrl.createProduct)
+router.post('/', [authJwt.verifyToken, authJwt.isModerator], productsCtrl.createProduct)
 router.get('/', productsCtrl.getProducts)
 router.get('/:productId', productsCtrl.getProductsById)
-router.put('/:productId', productsCtrl.updateProductById)
-router.delete('/:productId', productsCtrl.deleteProductById)
+router.put('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.updateProductById)
+router.delete('/:productId', [authJwt.verifyToken, authJwt.isAdmin], productsCtrl.deleteProductById)
 
 
 export default router
